27c1f15d6706ce7abe1e83b43c5265a6f04ef5c8
JeffW <116536323+Nxlq@users.noreply.github.com>
2024-02-20 15:19:32 -0500

    create and style the view template for the index.ejs page
    index page now renders information from the messages array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    name: "Bernie",
+    message: "Yo Reggie you down to ditch class tomorrow? 🚬",
+    date: new Date(),
+  },
+  {
+    name: "Reggie",
+    message: "Sure whatever idgaf 🤷‍♂️",
+    date: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Bernie and Reggie's Hideout" });
+  res.render("index", {
+    title: "Bernie and Reggie's Hideout",
+    messages: messages,
+  });
 });
 
 module.exports = router;
